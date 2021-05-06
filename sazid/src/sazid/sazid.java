47cf4b9724a2df60f9f2e3ba29db340a07310572
Sazid shariar <74678979+sazidshariar@users.noreply.github.com>
2021-05-06 23:57:56 +0600

    Add files via upload

---
+++ sazid/src/sazid/sazid.java
@@ -0,0 +1,16 @@
+package sazid;
+
+public class sazid {
+	public static void main(String[] args) 
+	 { 
+	 System.out.println("\n Prog. is showing even no"); 
+	 for(int i=2;i<=60;i++) 
+	 { 
+	 if(i%2==0) 
+	 { 
+	 System.out.print("\n "+i); 
+	 } 
+	 
+	 } 
+	 } 
+	}
